4b210e84b769b17517203d351a452f775bc74723
钟履桂 <zhonglvgui@magicengine.com.cn>
2025-03-18 13:43:18 +0800

    build: tw7

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-    },
+  swcMinify: false,
+  compress: false,
+  optimize: false, // 假设存在这个选项，但需要确认
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimize = false;
+      config.optimization.minimizer = [];
+    }
+    return config;
   },
 };
 
