c60d5ea76232d7a49231dafe9266f488d8a29ea1
钟履桂 <zhonglvgui@magicengine.com.cn>
2025-03-18 13:07:59 +0800

    build: tw

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "build": {
-    "env": {
-      "TAILWIND_USE_OXIDE": "1"
-    }
-  }
 };
 
 export default nextConfig;
