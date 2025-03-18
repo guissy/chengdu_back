6d26afbba9ac34902d9221fb0c5a5971257dadc4
钟履桂 <zhonglvgui@magicengine.com.cn>
2025-03-18 13:14:28 +0800

    build: tw3

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { queryClient } from "../lib/api/queryClient";
 import ClientComponent from "./(client)/ClientComponent";
-import client from "@/lib/api/client";
+// import client from "@/lib/api/client";
 
 export default async function HomePage() {
   // await queryClient.prefetchQuery({
